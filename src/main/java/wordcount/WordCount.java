34659d696a9e914d0f32a3b66a65dec8112c61dd
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2015-10-07 13:30:37 +0200

    implement wordcount

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,7 +1,38 @@
 package wordcount;
 
-/**
- * Created by kristinesundtlorentzen on 7/10/15.
- */
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCount {
+
+    public static void main( String[] args ) {
+
+        try {
+
+            final Job job = Job.getInstance( new Configuration() );
+
+            job.setJarByClass(WordCount.class);
+            job.setJobName("DBLP word counter");
+
+            FileInputFormat.addInputPath( job, new Path( args[ 0 ] ) );
+            FileOutputFormat.setOutputPath( job, new Path( args[ 1 ] ) );
+
+            job.setMapperClass(WordCountMapper.class);
+            job.setReducerClass(WordCountReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
--- src/main/java/wordcount/WordCountMapper.java
+++ src/main/java/wordcount/WordCountMapper.java
@@ -1,7 +1,64 @@
 package wordcount;
 
-/**
- * Created by kristinesundtlorentzen on 7/10/15.
- */
-public class WordCountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable ONE = new IntWritable(1);
+    private final Text WORD = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String text = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer( text );
+
+        while(tokenizer.hasMoreTokens()) {
+
+            WORD.set( tokenizer.nextToken() );
+
+            if( isWord( WORD.toString() ) ) {
+
+                WORD.set( new Text( cleanWord( WORD.toString() ) ) );
+                context.write( WORD, ONE );
+            }
+        }
+    }
+
+
+    /**
+     * Checks if a given string is a word, by checking that the first character of the string
+     * is a letter.
+     *
+     * @param key   The word to be checked
+     * @return      Whether or not the string is a word
+     */
+    private boolean isWord( String key ) {
+
+        return Character.isLetter( key.charAt( 0 ) );
+    }
+
+    /**
+     * Iterates through all the characters in a word, and removes any that are not letters.
+     *
+     * @param word
+     * @return      A version of word without any characters that are not letters
+     */
+    private String cleanWord( String word ) {
+
+        String cleanWord = "";
+
+        for( char c : word.toCharArray() ) {
+
+            cleanWord = Character.isLetter( c ) ? cleanWord + c : cleanWord;
+        }
+
+        return cleanWord;
+    }
 }
